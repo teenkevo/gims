c4ae837d0a9a1adc1c9a10902aeb4c693519eb39
Kevin Mugumya <luwembamugumya@gmail.com>
2025-02-02 03:33:01 +0300

    refactor: added error handling to sanity token

--- src/sanity/lib/backendClient.ts
+++ src/sanity/lib/backendClient.ts
@@ -1,11 +1,11 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from "../env";
+import { apiVersion, dataset, projectId, sanityToken } from "../env";
 
 export const backendClient = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token: process.env.SANITY_API_TOKEN,
+  token: sanityToken,
 });
