ea58489d8f104ea6b40337beb7d51a056e6c19f2
Kevin Mugumya <luwembamugumya@gmail.com>
2025-02-15 04:40:08 +0300

    feat: Enable full URL logging for fetch requests in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default withSentryConfig(nextConfig, {
