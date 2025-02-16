c313c71a0969cf8a5b54338044db62c2034cfaaf
Kevin Mugumya <luwembamugumya@gmail.com>
2025-02-17 01:28:09 +0300

    feat: Add initial opacity to client list item animations

--- src/features/internal/projects/components/project-details.tsx
+++ src/features/internal/projects/components/project-details.tsx
@@ -208,6 +208,7 @@ export default function ProjectDetails({
                   <motion.div
                     key={client._id}
                     layout="position"
+                    initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                   >
